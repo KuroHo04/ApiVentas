324ac52d14746e523f32434d41827ffc6eaee9dd
Jose Javier Mendoza Villalpando <Jose_Mendoza23@unikino.edu.mx>
2024-08-07 08:04:17 -0700

    creando controller y entidad de productos.

---
+++ ApiVentas/Controllers/ProductosController.cs
@@ -0,0 +1,31 @@
+﻿using ApiVentas.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiVentas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductosController : ControllerBase
+    {
+        List<Producto> _productos;
+
+        public ProductosController()
+        {
+            _productos = new List<Producto>();
+
+            _productos.Add(new Producto()
+            {
+                Id = 1,
+                Codigo = 1,
+                Descripcion = "Rockaccino",
+                PrecioUnitario = 70.00m
+            });
+        }
+
+        [HttpGet]
+        public  ActionResult<List<Producto>> ObtenerProductos()
+        {
+            return Ok(_productos);
+        }
+    }
+}
---
+++ ApiVentas/Entities/Producto.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiVentas.Entities
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+
+        public int Codigo { get; set; }
+
+        public string Descripcion { get; set; }
+
+        public decimal PrecioUnitario { get; set; }
+    }
+}
